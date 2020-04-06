6675b4a2025a443ca09f523f53f08dfd80d039c0
masterfermin02 <fperdomo@intellisys.com.do>
2020-04-05 23:42:21 -0400

    Added deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,5 @@ set -e
 # build
 npm run build
 
-# if you are deploying to a custom domain
-#echo 'www.coronavirus-rd.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
 # if you are deploying to firebase
 firebase deploy
